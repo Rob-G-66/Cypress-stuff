8669802518872bd38a3e05e0ac2fad989fad8d7f
615594345 <sam.2.greenwood@bt.com>
2025-06-23 11:44:50 +0100

    POM implemented where possible

--- cypress/e2e/1-getting-started/robs-first-test.cy.js
+++ cypress/e2e/1-getting-started/robs-first-test.cy.js
@@ -1,46 +1,38 @@
+import NBSLogin from '../../support/page-objects/nbs-login';
+
 describe('example to-do app', () => {
     it('Navigate to the Source website', () => {
-        cy.visit('https://login.thenbs.com/auth/login/');
-
-        cy.get('#Identification_Email')
-            .type('robert@robert-graham.org.uk');
-
-        cy.get('.submit-button')
-            .click();
-
-        cy.get('#Authentication_Password')
-            .type('NBSPassword!');
-
-        cy.get('#nextButton')
-            .click();
-
-          
-        // After login, navigate to the new origin
-        cy.visit('https://source.thenbs.com/');
-        
+        NBSLogin.loginToNBS('robert@robert-graham.org.uk', 'NBSPassword!');
 
         cy.origin('https://source.thenbs.com', () => {
+            // Define helper functions inside the callback if you want to keep things tidy
+            function acceptCookies() {
+                cy.contains('button', 'Accept All Cookies').click();
+            }
+            function searchFor(term) {
+                cy.get('[data-cy="searchFieldSearch"]').first().type(term);
+            }
+            function selectDysonResult() {
+                cy.contains('Dyson', { timeout: 10000 }).should('be.visible').click();
+            }
+            function verifyDysonPage() {
+                cy.url().should('include', '/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview');
+                cy.get('h1.ng-star-inserted').should('have.text', 'Dyson');
+            }
+            function verifyContactNumber() {
+                cy.get('a[href="tel:08003457788"]').should('be.visible').should('have.text', ' 08003457788 ');
+            }
+            function verifyWebsiteLink() {
+                cy.get('a[href="https://www.dyson.co.uk/commercial/overview/architects-designers"]').should('be.visible').should('have.text', ' Website ');
+            }
 
-            cy.contains('button', 'Accept All Cookies').click();
-            // Within the search field, type Dyson
-            cy.get('[data-cy="searchFieldSearch"]').first().type('Dyson');
-            // Verify that the search results contains Dyson and select it
-            cy.contains('Dyson', { timeout: 10000 }).should('be.visible').click();
-
-            // Assert the URL includes the expected path
-            cy.url().should('include', '/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview');
-            // Assert that Dyson text on page is sized Heading h1 and is visible
-            cy.get('h1.ng-star-inserted').should('have.text', 'Dyson');
-            // Assert that contact number link on page = 08003457788
-            cy.get('a[href="tel:08003457788"]').should('be.visible').should('have.text', ' 08003457788 ');
-            //cy.get('a[href="https://www.dyson.co.uk/commercial/overview/architects-designers"]').should('be.visible');
-            cy.get('a[href="https://www.dyson.co.uk/commercial/overview/architects-designers"]').should('be.visible').should('have.text', ' Website ');         
-
-        
-
+            // Use the helpers
+            acceptCookies();
+            searchFor('Dyson');
+            selectDysonResult();
+            verifyDysonPage();
+            verifyContactNumber();
+            verifyWebsiteLink();
         });
-
-
     });
-    
 });
