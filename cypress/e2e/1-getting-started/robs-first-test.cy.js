31c3cad9550fa3c83963a093dde37bbaa21bfecf
615594345 <sam.2.greenwood@bt.com>
2025-06-24 09:35:03 +0100

    Added comments and true POM

--- cypress/e2e/1-getting-started/robs-first-test.cy.js
+++ cypress/e2e/1-getting-started/robs-first-test.cy.js
@@ -1,25 +1,34 @@
 import NBSHomepage from '../../support/page-objects/nbs-homepage';
 import DysonHomepage from '../../support/page-objects/dyson-homepage';
 
+// Main test suite for NBS Source regression tests
 describe('NBS Source Regression Tests', () => {
+    // Runs before each test to set up the initial state
     beforeEach(() => {
+        // Visit the NBS Source homepage
         cy.visit('https://source.thenbs.com');
 
+        // Accept cookies on the homepage
         NBSHomepage.acceptCookies();
-        NBSHomepage.searchFor('Dyson');
-        NBSHomepage.selectDysonResult();
 
+        // Search for 'Dyson' using the homepage search functionality
+        NBSHomepage.searchFor('Dyson');
+
+        // Select the Dyson result from the search results
+        NBSHomepage.selectDysonResult();
     });
 
+    // Test to verify the Dyson page URL and H1 text
     it('Should verify Dyson page URL, and H1 text', () => {
         DysonHomepage.verifyDysonPage
-
     });
 
+    // Test to verify the contact number on the Dyson page
     it('Should verify contact number', () => {
         DysonHomepage.verifyContactNumber();
     });
 
+    // Test to verify the website link on the Dyson page
     it('Should verify website link', () => {
         DysonHomepage.verifyWebsiteLink();
     });
--- cypress/support/page-objects/dyson-homepage.js
+++ cypress/support/page-objects/dyson-homepage.js
@@ -1,29 +1,36 @@
+// Page Object Model for the Dyson manufacturer page on NBS Source
 class DysonHomepage {
-    // Selectors as class properties
-    dysonUrlPart = '/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview';
-    dysonHeader = 'h1.ng-star-inserted';
-    contactNumberLink = 'a[href="tel:08003457788"]';
-    websiteLink = 'a[href="https://www.dyson.co.uk/commercial/overview/architects-designers"]';
+    // Selectors for elements on the Dyson page
+    dysonUrlPart = '/manufacturer/dyson/nakAxHWxDZprdqkBaCdn4U/overview'; // Partial URL to identify Dyson page
+    dysonHeader = 'h1.ng-star-inserted'; // Selector for the main page header (H1)
+    contactNumberLink = 'a[href="tel:08003457788"]'; // Selector for the contact phone number link
+    websiteLink = 'a[href="https://www.dyson.co.uk/commercial/overview/architects-designers"]'; // Selector for the Dyson website link
 
-    // Actions / Assertions
+
+    // Actions
+    //-------------
+    // Verifies that the current URL and page header are correct for Dyson
     verifyDysonPage() {
-        cy.url().should('include', this.dysonUrlPart);
-        cy.get(this.dysonHeader).should('have.text', 'Dyson');
+        cy.url().should('include', this.dysonUrlPart); // Check URL contains Dyson path
+        cy.get(this.dysonHeader).should('have.text', 'Dyson'); // Check H1 text is 'Dyson'
     }
 
+    // Verifies the contact number link is visible, has correct text, and correct tel: protocol
     verifyContactNumber() {
         cy.get(this.contactNumberLink)
-            .should('be.visible')
-            .should('have.text', ' 08003457788 ');
-            //we can also verify that the correct telephone protocol is in place for this
-            cy.get(this.contactNumberLink).should('have.attr', 'href', 'tel:08003457788');
+            .should('be.visible') // Ensure the contact number is visible
+            .should('have.text', ' 08003457788 '); // Ensure the text matches the expected number
+        // Additionally, verify the href uses the correct telephone protocol
+        cy.get(this.contactNumberLink).should('have.attr', 'href', 'tel:08003457788');
     }
 
+    // Verifies the website link is visible and has the correct text
     verifyWebsiteLink() {
         cy.get(this.websiteLink)
-            .should('be.visible')
-            .should('have.text', ' Website ');
+            .should('be.visible') // Ensure the website link is visible
+            .should('have.text', ' Website '); // Ensure the link text is 'Website'
     }
 }
 
+// Export a singleton instance of the DysonHomepage class
 export default new DysonHomepage();
